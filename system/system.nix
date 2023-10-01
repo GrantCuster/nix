7ccf6d304894280457f5941a8baa7ab21ec83a15
Grant Custer <grantcuster@gmail.com>
2023-10-01 10:10:36 -0400

    update

--- system/system.nix
+++ system/system.nix
@@ -50,7 +50,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "bix"; # Define your hostname.
 
   networking.networkmanager.enable = true;
 
